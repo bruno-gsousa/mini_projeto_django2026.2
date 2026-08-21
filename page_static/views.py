61f48e5ceca9225672a288da73629277671e77eb
bruno-gsousa <bruno.gsousa.senac@gmail.com>
2026-08-21 16:18:39 -0300

    links_funcionais

--- page_static/views.py
+++ page_static/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Essa é view de teste</h1>")
+    return render(request,'index.html')
 
 def contatos(request):
-    return HttpResponse("<p>telefone:(21)96546-64564</p><p>Email:seo@gmail.com</p>")
+    return render(request,'contatos.html')
+
+def servicos(request):
+    return render(request,'servicos.html')
+
+def sobre(request):
+    return render(request,'sobre.html')
